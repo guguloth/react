711013d431a5bd536fe096ee406e117a045a7d14
GOPAL NAIK GUGULOTH <gugulothgopalnaik@gmail.com>
2022-02-22 23:21:27 +0530

    liftingUpState_props_components

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
               Ristorante con
             </NavbarBrand>
         </Navbar>
-        <Menu dishes/>
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
